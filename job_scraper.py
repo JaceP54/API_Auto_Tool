d7a95d49e7ecdbc96abf9c8327b2454048e91d23
Jace Pennycuff <pennycuffjace@gmail.com>
2025-08-03 08:56:40 -0400

    Updated script to know allow keyword search through the CSV. Also added a time stamp function to run after each new pull.

--- job_scraper.py
+++ job_scraper.py
@@ -2,9 +2,11 @@ import requests
 import json
 import pandas as pd
 import csv
+from datetime import datetime
 
 response_API = requests.get('https://remoteok.com/api')
 data = response_API.json()
+
 
 # Get the first 5 actual job listings (skipping metadata at index 0)
 jobs = data[1:6]
@@ -27,13 +29,26 @@ parsed = pd.DataFrame([
 ])
 parsed.to_csv("remote_jobs_parsed.csv", index=False)
 
-
-
 for job in jobs:
     slug = job.get('position', "N/A")
     company = job.get("company", "N/A")
     url = job.get("url", "N/A")
 
-    print(f"{slug} at {company} — {url}")
+keyword = input("Enter a keyword to find a job listing: ").lower()
+found_rows = []
 
-#keyword = input("Enter a keyword to find a job listing ")
+
+with open('remote_jobs_parsed.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if keyword in row['Position'].lower() or keyword in row['Tags'].lower():
+            found_rows.append(row)
+
+print(f"\nJobs matching {keyword}:\n")
+for row in found_rows:
+    print(f"{row['Position']} at {row['Company']} — {row['URL']}")
+
+filename = f"jobs_{keyword}_{datetime.now().strftime('%Y-%m-%d')}.csv"
+pd.DataFrame(found_rows).to_csv(filename, index=False)
+
+print(filename)
