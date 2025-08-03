d32bf63e9563239b9d2db6ac73642ddff7b42d48
Jace Pennycuff <pennycuffjace@gmail.com>
2025-08-03 08:21:58 -0400

    Job  scraper now pulls API Data and prints the raw data from JSON to xlsx. The Scraper is also pulling and parsing to display the first five jobs title, company, and URL.

--- job_scraper.py
+++ job_scraper.py
@@ -1,5 +1,28 @@
 import requests
-import _json
+import json
+import pandas as pd
 
 response_API = requests.get('https://remoteok.com/api')
-print(response_API.status_code)
+data = response_API.json()
+
+# Get the first 5 actual job listings (skipping metadata at index 0)
+jobs = data[1:6]
+
+raw_json_strings = [json.dumps(job, indent=2) for job in data[1:6]]  # skipping metadata
+
+# Create a DataFrame with one column
+df = pd.DataFrame(raw_json_strings, columns=["Raw API Data"])
+
+# Save to Excel
+file_name = "remote_job_raw.xlsx"
+df.to_excel(file_name, index=False)
+
+
+for job in jobs:
+    slug = job.get('position', "N/A")
+    company = job.get("company", "N/A")
+    url = job.get("url", "N/A")
+
+    print(f"{slug} at {company} — {url}")
+
+keyword = input("Enter a keyword to find a job listing ")
